c75f5cf60c62440805f91c203d425ff11c4828fe
Cody Miller <hello@looskie.com>
2022-07-10 19:55:46 -0400

    feat: entire website

---
+++ components/Navbar.tsx
@@ -0,0 +1,94 @@
+import styled from "styled-components";
+import { GITHUB_REPO, TWITTER } from "../utils/Consts";
+import { GitHubIcon, TwitterIcon } from "./Icons";
+
+const Container = styled.div`
+  display: flex;
+  width: 80vw;
+  align-items: center;
+  position: fixed;
+  align-self: center;
+  height: 60px;
+  padding: 10px;
+  top: 0;
+  left: 0;
+  margin: 0 10%;
+  font-size: 1.15em;
+  background: hsl(35, 31%, 88%, 80%);
+  backdrop-filter: blur(3px);
+  z-index: 9999;
+`;
+
+const Title = styled.h1`
+  left: 0;
+  font-size: 1.25em;
+  font-weight: bold;
+  color: var(--text-secondary);
+`;
+
+const StyledLinks = styled.ul`
+  display: flex;
+  gap: 10px;
+  list-style: none;
+  font-weight: 900;
+  margin-left: 20px;
+  flex: 1;
+
+  li {
+    transition: 0.15s ease-in-out;
+    color: var(--text-muted);
+
+    &:hover {
+      color: var(--text-secondary);
+    }
+  }
+`;
+
+const Socials = styled.div`
+  display: flex;
+  gap: 10px;
+  justify-self: right;
+
+  svg {
+    display: block;
+    height: 25px;
+    color: var(--text-muted);
+
+    transition: 0.15s ease-in-out;
+
+    &:hover {
+      color: var(--text-secondary);
+    }
+  }
+`;
+
+const Navbar = () => {
+  return (
+    <Container>
+      <Title>Capybara</Title>
+      <StyledLinks>
+        <li>
+          <a href="#">home</a>
+        </li>
+        <li>
+          <a href="#documentation">documentation</a>
+        </li>
+        <li>
+          <a href={TWITTER} target="_blank" rel="noreferrer">
+            twitter
+          </a>
+        </li>
+      </StyledLinks>
+      <Socials>
+        <a href={GITHUB_REPO} target="_blank" rel="noreferrer">
+          <GitHubIcon />
+        </a>
+        <a href={TWITTER} target="_blank" rel="noreferrer">
+          <TwitterIcon />
+        </a>
+      </Socials>
+    </Container>
+  );
+};
+
+export default Navbar;
---
+++ utils/Consts.ts
@@ -0,0 +1,93 @@
+export type Testimonial = {
+  name: string;
+  worksAt: string;
+  pfp: string;
+  message: string;
+};
+
+export const BASE_API = "https://api.capy.lol";
+export const BASE_API_V1 = `${BASE_API}/v1`;
+
+export const GITHUB_REPO = "https://github.com/looskie/capybara-web";
+export const GITHUB_API_REPO = "https://github.com/looskie/capybara-api";
+export const TWITTER = "https://twitter.com/devlooskie";
+
+export const TESTIMONIALS: Testimonial[] = [
+  {
+    message:
+      " Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reiciendis voluptatibus natus blanditiis provident eveniet rerum distinctio obcaecati facere accusantium minus.",
+    name: "username",
+    pfp: "https://api.capy.lol/v1/capybara",
+    worksAt: "Hop inc.",
+  },
+  {
+    message:
+      " Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reiciendis voluptatibus natus blanditiis provident eveniet rerum distinctio obcaecati facere accusantium minus.",
+    name: "username",
+    pfp: "https://api.capy.lol/v1/capybara",
+    worksAt: "Hop inc.",
+  },
+  {
+    message:
+      " Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reiciendis voluptatibus natus blanditiis provident eveniet rerum distinctio obcaecati facere accusantium minus.",
+    name: "username",
+    pfp: "https://api.capy.lol/v1/capybara",
+    worksAt: "Hop inc.",
+  },
+  {
+    message:
+      " Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reiciendis voluptatibus natus blanditiis provident eveniet rerum distinctio obcaecati facere accusantium minus.",
+    name: "username",
+    pfp: "https://api.capy.lol/v1/capybara",
+    worksAt: "Hop inc.",
+  },
+  {
+    message:
+      " Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reiciendis voluptatibus natus blanditiis provident eveniet rerum distinctio obcaecati facere accusantium minus.",
+    name: "username",
+    pfp: "https://api.capy.lol/v1/capybara",
+    worksAt: "Hop inc.",
+  },
+  {
+    message:
+      " Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reiciendis voluptatibus natus blanditiis provident eveniet rerum distinctio obcaecati facere accusantium minus.",
+    name: "username",
+    pfp: "https://api.capy.lol/v1/capybara",
+    worksAt: "Hop inc.",
+  },
+  {
+    message:
+      " Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reiciendis voluptatibus natus blanditiis provident eveniet rerum distinctio obcaecati facere accusantium minus.",
+    name: "username",
+    pfp: "https://api.capy.lol/v1/capybara",
+    worksAt: "Hop inc.",
+  },
+  {
+    message:
+      " Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reiciendis voluptatibus natus blanditiis provident eveniet rerum distinctio obcaecati facere accusantium minus.",
+    name: "username",
+    pfp: "https://api.capy.lol/v1/capybara",
+    worksAt: "Hop inc.",
+  },
+  {
+    message:
+      " Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reiciendis voluptatibus natus blanditiis provident eveniet rerum distinctio obcaecati facere accusantium minus.",
+    name: "username",
+    pfp: "https://api.capy.lol/v1/capybara",
+    worksAt: "Hop inc.",
+  },
+  {
+    message:
+      " Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reiciendis voluptatibus natus blanditiis provident eveniet rerum distinctio obcaecati facere accusantium minus.",
+    name: "username",
+    pfp: "https://api.capy.lol/v1/capybara",
+    worksAt: "Hop inc.",
+  },
+  {
+    message:
+      " Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reiciendis voluptatibus natus blanditiis provident eveniet rerum distinctio obcaecati facere accusantium minus.",
+    name: "username",
+    pfp: "https://api.capy.lol/v1/capybara",
+    worksAt: "Hop inc.",
+  },
+];
